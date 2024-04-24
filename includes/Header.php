f45fe79476cd5371d145d5c01d9ae2e55d7af629
DevSteven1 <stevenpajarodev@gmail.com>
2024-04-24 01:29:02 -0500

    Header_1

---
+++ includes/Header.php
@@ -0,0 +1,3 @@
+<?php
+    echo '<header class="flex justify-center items-center h-28 bg-black text-white text-3xl w-full border rounded-b-lg">Ichiraku Ramen</header>';
+?>
--- index.php
+++ index.php
@@ -8,42 +8,48 @@
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.css" />
     <script src="https://cdn.tailwindcss.com"></script>
 </head>
+
 <body class="h-screen flex flex-col m-0 p-0">
 
-    <header class="h-20 bg-black w-full"></header>
-
+<?php require_once './includes/Header.php'; ?>
     <section class="swiper mySwiper h-full w-full">
         <div class="swiper-wrapper">
             <?php
-            include './includes/Card.php';
 
-            $elementos = array(
-                '{"titulo": "Elemento 1", "precio": "100.000"}',
-                '{"titulo": "Elemento 1", "precio": "100.000"}',
-                '{"titulo": "Elemento 1", "precio": "100.000"}',
-                '{"titulo": "Elemento 1", "precio": "100.000"}',
-                '{"titulo": "Elemento 1", "precio": "100.000"}',
-                '{"titulo": "Elemento 1", "precio": "100.000"}',
-                '{"titulo": "Elemento 1", "precio": "100.000"}',
-                '{"titulo": "Elemento 1", "precio": "100.000"}',
-            );
+            $estado = 0;
 
-            $grupos = array_chunk($elementos, 6);
+            if ($estado == 0) {
 
-            foreach ($grupos as $grupo) {
-                echo '<div class="swiper-slide grid grid-cols-3 grid-rows-2 p-10 gap-10">';
-                foreach ($grupo as $elemento) {
-                    $objeto = json_decode($elemento);
-                    Card($objeto->titulo, $objeto->precio);
+                include './includes/Card.php';
+
+                $elementos = array(
+                    '{"titulo": "Elemento 1", "precio": "100.000"}',
+                    '{"titulo": "Elemento 1", "precio": "100.000"}',
+                    '{"titulo": "Elemento 1", "precio": "100.000"}',
+                    '{"titulo": "Elemento 1", "precio": "100.000"}',
+                    '{"titulo": "Elemento 1", "precio": "100.000"}',
+                    '{"titulo": "Elemento 1", "precio": "100.000"}',
+                    '{"titulo": "Elemento 1", "precio": "100.000"}',
+                    '{"titulo": "Elemento 1", "precio": "100.000"}',
+                );
+
+                $grupos = array_chunk($elementos, 6);
+
+                foreach ($grupos as $grupo) {
+                    echo '<div class="swiper-slide grid grid-cols-3 grid-rows-2 p-10 gap-10">';
+                    foreach ($grupo as $elemento) {
+                        $objeto = json_decode($elemento);
+                        Card($objeto->titulo, $objeto->precio);
+                    }
+                    echo '</div>';
                 }
-                echo '</div>';
             }
             ?>
         </div>
         <div class="swiper-pagination"></div>
     </section>
 
-    <footer class="h-20 bg-black w-full"></footer>
+    <footer class="h-32 bg-black w-full border rounded-t-lg"></footer>
 
     <script src="https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.js"></script>
 
