a81f57c0a6553d49495ced90d0db807bc2a62b21
DevSteven1 <stevenpajarodev@gmail.com>
2024-04-23 22:04:29 -0500

    User-Menu_Cards

--- index.php
+++ index.php
@@ -10,31 +10,31 @@
 </head>
 <body class="h-screen flex flex-col m-0 p-0">
 
-    <header class="h-20 bg-green-300 w-full"></header>
+    <header class="h-20 bg-black w-full"></header>
 
     <section class="swiper mySwiper h-full w-full">
         <div class="swiper-wrapper">
             <?php
             include './includes/Card.php';
-            
+
             $elementos = array(
-                '{"titulo": "Elemento 1", "contenido": "Lorem ipsum dolor sit amet, consectetur adipiscing elit."}',
-                '{"titulo": "Elemento 2", "contenido": "Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas."}',
-                '{"titulo": "Elemento 3", "contenido": "Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."}',
-                '{"titulo": "Elemento 4", "contenido": "Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."}',
-                '{"titulo": "Elemento 5", "contenido": "Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur."}',
-                '{"titulo": "Elemento 6", "contenido": "Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."}',
-                '{"titulo": "Elemento 7", "contenido": "Lorem ipsum dolor sit amet, consectetur adipiscing elit."}',
-                '{"titulo": "Elemento 8", "contenido": "Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas."}'
+                '{"titulo": "Elemento 1", "precio": "100.000"}',
+                '{"titulo": "Elemento 1", "precio": "100.000"}',
+                '{"titulo": "Elemento 1", "precio": "100.000"}',
+                '{"titulo": "Elemento 1", "precio": "100.000"}',
+                '{"titulo": "Elemento 1", "precio": "100.000"}',
+                '{"titulo": "Elemento 1", "precio": "100.000"}',
+                '{"titulo": "Elemento 1", "precio": "100.000"}',
+                '{"titulo": "Elemento 1", "precio": "100.000"}',
             );
 
             $grupos = array_chunk($elementos, 6);
 
             foreach ($grupos as $grupo) {
-                echo '<div class="swiper-slide grid grid-cols-3 grid-rows-2">';
+                echo '<div class="swiper-slide grid grid-cols-3 grid-rows-2 p-10 gap-10">';
                 foreach ($grupo as $elemento) {
                     $objeto = json_decode($elemento);
-                    Card($objeto->titulo, $objeto->contenido);
+                    Card($objeto->titulo, $objeto->precio);
                 }
                 echo '</div>';
             }
@@ -43,7 +43,7 @@
         <div class="swiper-pagination"></div>
     </section>
 
-    <footer class="h-20 bg-green-300 w-full"></footer>
+    <footer class="h-20 bg-black w-full"></footer>
 
     <script src="https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.js"></script>
 
@@ -52,6 +52,12 @@
             pagination: {
                 el: ".swiper-pagination",
             },
+        });
+
+        var bullets = document.querySelectorAll('.swiper-pagination-bullet');
+        bullets.forEach(function(bullet) {
+            bullet.classList.remove('bg-blue-500');
+            bullet.classList.add('bg-black');
         });
     </script>
 </body>
